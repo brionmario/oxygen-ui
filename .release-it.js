72f0d7062acd606cc62753a4caeb8b4e2c2b71c9
Brion <info@brionmario.com>
2023-03-22 20:05:08 +0530

    fix: experiment with release

--- .release-it.js
+++ .release-it.js
@@ -15,9 +15,6 @@ module.exports = {
     "@release-it/conventional-changelog": {
       "infile": "CHANGELOG.md",
       "header": "# Changelog",
-      writerOpts: {
-        commitPartial: commitTemplate
-      },
       "gitRawCommitsOpts": {
         format: '%B%n-hash-%n%H%n-gitTags-%n%d%n-committerDate-%n%ci%n-authorName-%n%an%n-authorEmail-%n%ae%n-gpgStatus-%n%G?%n-gpgSigner-%n%GS'
       },
